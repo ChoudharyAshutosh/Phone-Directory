1d755c16b738a4d8389b8ef56751580a34df0ab8
Ashutosh <choudharyashutosh7070@gmail.com>
2020-12-03 14:07:35 +0530

    Style sheet created for home page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import {Fragment} from 'react';
-function App() {
+import React,{Fragment,Component} from 'react';
+import Header from './Header';
+import './Formating.css'
+class App extends Component {
+
+  render(){
+    let subscribers=[{
+      name : "Ashutosh Choudhary",
+      phone : "1234567890"
+    },
+    {
+      name : "Ankit Chauhan",
+      phone : "1234567891"
+    }
+  ]
   return (
     <Fragment>
-      <div>
-      Phone Directory
-      </div>
-      <button>Add</button>
-      <div>
-        <span>Name</span><br/>
-        <span>Phone</span>
-      </div>
+      <Header/>
+      <div className="container-body">
+      <button className="success">Add</button>
+      <div className="container">
+        <span className="container-item heading">Name</span>
+        <span className="container-item heading">Phone</span>
+        </div>
+        {subscribers.map(sub=>{
+        return <div className="container">
+          <span className="container-item name">{sub.name}</span>
+          <span className="container-item phone">{sub.phone}</span>
+          <button className="danger">Delete</button>
+        </div>
+      }
+
+      )
+
+      }
+    
+     </div>
     </Fragment>
   );
-}
+}}
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,25 @@
+//import React, {Component} from 'react'
+import React from 'react'
+import './Header.css'
+const Header= function(){
+    // const headerStyle={textAlign:'center',
+    // padding:'20px',
+    // background:'#000',
+    // color:'#fff',
+    // textTransform:'uppercase'}
+    return(
+        <div className="header">
+            Phone Directory
+        </div>
+    );
+}
+//class Header extends Component{
+//    render(){
+//        return(
+//            <div className="header">
+//                Phone directory
+//            </div>
+//        )
+//    }
+//}
+export default Header;
